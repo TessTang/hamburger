b7794651be69f93b676c7f7eec03ef610b8ce0b1
TessTang <z8937200@gmail.com>
2024-02-24 16:33:21 +0800

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,22 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { NavLink } from "react-router-dom";
 import { CartData } from "../store/cartStore"
 
 
 export default function Navbar() {
 
+    const [roll, setRoll] = useState(false)
     const { cart } = useContext(CartData);
 
     return (
-        <nav className="row navbar navbar-light fixed-top myNavbar p-2">
+        <nav className="row navbar navbar-light fixed-top myNavbar p-2 active">
             <div className="navbarSection col-3"/>
             <div className="navbarSection col-6 justify-content-between">
             <NavLink className="nav-item nav-link" to='/'>首頁</NavLink>
-            <NavLink className="navbar-brand navbarLogo me-0">Navbar</NavLink>
+            <NavLink className="navbar-brand navbarLogo me-0"><img src={require('../assets/logo.png')} width="30" height="30" alt="123" /></NavLink>
             <NavLink className="nav-item nav-link" to='/products'>產品</NavLink>
             </div>
-            <div className="navbarSection col-3 justify-content-end gap-3">
+            <div className="navbarSection col-3 justify-content-end gap-4">
                 <NavLink className="nav-item nav-link  position-relative" to='/cart'>
                     <i className="bi bi-bag-fill fs-5"></i>
                     <span className="position-absolute top-10 start-100 translate-middle badge rounded-pill bg-danger">
